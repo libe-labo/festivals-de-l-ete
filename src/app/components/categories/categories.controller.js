d2aa53abd039b6a5947977455f5bbe080f3f0323
Paul Joannon <hello@pauljoannon.com>
2015-06-18 19:06:49 +0200

    create a 'categories' component

---
+++ src/app/components/categories/categories.controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('festivals').controller('CategoriesCtrl', function ($scope, $rootScope) {
+    $scope.categories = [
+        'Tous', 'BD', 'Cinéma', 'Danse', 'Littérature', 'Musique',
+        'Photo / Art contemporain', 'Theâtre / Arts de la rue / Cirque', 'Autres'
+    ];
+
+    /*
+    ** Handle buttons
+    */
+
+    var selectedCategory;
+
+    $scope.selectCategory = function(category) {
+        if (_.contains($scope.categories, category)) {
+            selectedCategory = category;
+            $rootScope.$broadcast('category:change',
+                                  selectedCategory === 'Tous' ? null : selectedCategory);
+        }
+    };
+
+    $scope.getCategoryClass = function(category) {
+        return {
+            'md-primary' : category === selectedCategory
+        };
+    };
+});
